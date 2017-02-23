e7dd9be1eb38e32398cb18c85889e4f89f48646d
petekal1323 <petekal1323@gmail.com>
2017-02-22 21:48:56 -0500

    added jquery stuff as a group and things hashtag blessed

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 
+	$(".seat").on("click", function(){
+		$("#form").show(); //This function makes our 
+							//form showup. hashtag blessed
+	});
+
+	$(".available").mouseenter (function(){
+		$(this).append("<span><br>seat available</span>");
+
+	});
+	$(".available").mouseleave (function(){
+		$(this).find("span:last").remove();
+
 	
 });
+	});
