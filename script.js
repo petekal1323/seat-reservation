f04e4022fa99b189de9f766cfeb5d95a575099bf
Karl Schlatzer <rkschlatzer@gmail.com>
2017-02-23 19:42:23 -0500

    Added Flexbox & Beyond
    Changed Everything

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	
+
+   $(".available").on("click", function(){
+        $("#form").show();
+				$(this).toggleClass("reserved")
+
+});
+
+
+/*
+ This stuff toggles adding text mouseenter/leave
+    $(".available").mouseenter(function() {
+
+
+
+						$(this).append("<p><br />Seat <br />Available</p>");
+			});
+
+    $(".available").mouseleave(function() {
+
+            $(this).find("p:last").remove();
+
+    });
+		*/
 });
