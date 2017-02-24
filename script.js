4f551f636b43600588e51c681ef167b62d28a5e3
petekal1323 <petekal1323@gmail.com>
2017-02-23 20:46:57 -0500

    back in business

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	$(".seat").on("click", function(){
-		$("#form").show(); //This function makes our
-							//form showup. hashtag blessed
-	});
 
-	$(".available").mouseenter (function(){
-		$(this).append("<span><br>seat available</span>");
-
-	});
-	$(".available").mouseleave (function(){
-		$(this).find("span:last").remove();
- /* New Comment*/
+   $(".available").on("click", function(){
+        $("#form").show();
+				$(this).toggleClass("reserved")
 
 });
-	});
+
+
+/*
+    $(".available").mouseenter(function() {
+
+
+						$(this).append("<p><br />Seat <br />Available</p>");
+			});
+
+    $(".available").mouseleave(function() {
+
+            $(this).find("p:last").remove();
+
+    });
+    */
+});
