f37a4830f3b1dd45c7754b772b702654cb1dc2ed
petekal1323 <petekal1323@gmail.com>
2017-02-23 21:55:17 -0500

    declaring stuff

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ $(document).ready(function() {
     
 
    var reservation = [];
-   var seatReserve;
+   var currentSeat;
 
     $(".btn").click(function(){
         var name = $("#name").text();
         $
-    })
+    });
+
+    function seatClick(){
+        var seatNumber;
+        $(".column").on("click", function(event){
+            currentSeat = this;
+            seatNumber = currentSeat.id;
+        });
+
+    $("#form").on("submit", function(event){
+        event.preventDefault();
+        var name = $("#name").val();
+        reservation.push(
+        {
+            name: name,
+            number: seatNumber}
+            );
+    });
+    
+
+
+    }
+
+
+
 
 });
