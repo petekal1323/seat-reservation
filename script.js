d50a7637fc467e042c93a83403ab4745107300f5
petekal1323 <petekal1323@gmail.com>
2017-02-25 17:08:37 -0500

    working with youReserve reset

--- script.js
+++ script.js
@@ -1,60 +1,71 @@
 $(document).ready(function() {
 
 
- $(".available").on("click", function(){
-    $("#form").slideDown();
-    $('html, body').animate({
-        scrollTop: $("#form").offset().top
-    }, 2000);
-    $(this).toggleClass("reserved");
-});
+	$(".available").on("click", function(){
+		$("#form").slideDown();
+		$('html, body').animate({
+			scrollTop: $("#form").offset().top
+		}, 2000);
+		$(this).toggleClass("reserved");
+	});
 
- 
+	
 
- var reservation = [];
- var currentSeat;
- $(".available").click(seatClick());
+	var reservation = [];
+	var currentSeat;
+	$(".available").click(seatClick());
 
- $(".btn").click(function(){
-    var name = $("#name").text();
-    $
-});
+	$(".btn").click(function(){
+		var name = $("#name").text();
+		$
+	});
 
- function seatClick(){
-    var seatNumber;
-    $(".available").on("click", function(event){
-        if ($(this).hasClass("available") === true) {
-            currentSeat = this;
-            seatNumber = currentSeat.id;
-        }
-        
-    });
+	function seatClick(){
+		var seatNumber;
+		$(".available").on("click", function(event){
+			if ($(this).hasClass("available") === true) {
+				currentSeat = this;
+				seatNumber = currentSeat.id;
+			}
+			
+		});
 
-    $("#form").on("submit", function(event){
-        event.preventDefault();
-        var name = $("#name").val();
-        reservation.push(
-        {
-            name: name,
-            number: seatNumber}
-            );	
-        $("#form").val("").hide();
-        $('#form').find('input:text').val(''); 
+		$("#form").on("submit", function(event){
+			event.preventDefault();
+			var name = $("#name").val();
+			reservation.push(
+			{
+				name: name,
+				number: seatNumber}
+				);	
+			$("#form").val("").hide();
+			$('#form').find('input:text').val(''); 
 
-        $(".reserved").addClass("submitted").removeClass("available");
-        
-        function ran(){
-            $("#youReserved").text("Reserved by ").append("<span> "+name+"</span>");
-            $(".reserved").addClass("submitted").removeClass("reserved:hover  available:hover column:hover reserved available");
-            $(".submitted").off("click");
-            
-        }
+			$(".reserved").addClass("submitted").removeClass("available");
+			
+			function ran(){
+				$("#youReserved").text(seatNumber + " Reserved by "+name).show();
+				$(".reserved").addClass("submitted").removeClass("reserved:hover  available:hover column:hover reserved available");
+				$(".submitted").off("click");
+				
+			}
 
-        if ($(".column").hasClass("submitted") === true ) {
-            ran();
-            
-        };
-    });
-}
+			if ($(".column").hasClass("submitted") === true ) {
+				ran();
+				
+			};
+		});
 
+	}
+
+	function reset(){
+		$("#youReserved").hide();
+	};
+
+	$(".column").on("click", function(event){
+		if (($(".column").hasClass("submitted") === true) && ($(".column").hasClass("available") === true)){
+			reset();
+		};
+	});
+	
 });
